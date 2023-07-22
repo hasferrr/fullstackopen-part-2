fda845e659f5abaf92d4322c6726ae8e317c5f1c
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-07-22 15:36:51 +0700

    finish 2.6

---
+++ phonebook/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({
+      name: newName
+    }))
+  }
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+    setNewName(newValue)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+export default App
