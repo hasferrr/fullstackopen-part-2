2a48ce75cf55895d6db4275f09727f2fcddcd3a1
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-07-24 18:19:59 +0700

    finish 2.13

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -12,10 +12,10 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   const hook = () => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then((response) => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(allPersonData => {
+        setPersons(allPersonData)
       })
   }
   useEffect(hook, [])
@@ -31,10 +31,11 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-    axios
-      .post("http://localhost:3001/persons", newContact)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+
+    personService
+      .create(newContact)
+      .then(addedPerson => {
+        setPersons(persons.concat(addedPerson))
         setNewName('')
         setNewNumber('')
       })
